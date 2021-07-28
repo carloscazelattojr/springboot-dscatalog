c72dacc12999056f2a989fca838b4fe3bdd879e3
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-07-27 22:12:33 -0300

    H2 database in test

--- backend/src/test/java/br/com/carlosjunior/dscatalog/DscatalogApplicationTests.java
+++ backend/src/test/java/br/com/carlosjunior/dscatalog/DscatalogApplicationTests.java
@@ -1,13 +1,6 @@
 package br.com.carlosjunior.dscatalog;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class DscatalogApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+ 
 }
