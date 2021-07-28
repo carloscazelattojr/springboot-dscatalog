787e6c8cf74f0b0726245ff179d0503fc743391e
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-07-27 22:28:10 -0300

    Databse H2 seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into TB_CATEGORY(NAME) VALUES('Books');
+insert into TB_CATEGORY(NAME) VALUES('Eletronics');
+insert into TB_CATEGORY(NAME) VALUES('Papers');
+insert into TB_CATEGORY(NAME) VALUES('Games');
+insert into TB_CATEGORY(NAME) VALUES('Computers');
